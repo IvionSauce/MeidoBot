bcbc8ba01ae2083aa018bb605131d1cd0667a81b
Ivion <ivion@xs4all.nl>
2014-03-28 22:04:58 +0100

    Added finally block to dispose of WebClient.

--- UrlTitling/WebTools.cs
+++ UrlTitling/WebTools.cs
@@ -101,6 +101,10 @@ namespace WebHelp
             {
                 return new WebString(uri, ex);
             }
+            finally
+            {
+                wc.Dispose();
+            }
         }
     }
 
