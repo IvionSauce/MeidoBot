de923427e7e48bf371e73100cc8bf943e09baaf5
Ivion <ivion@xs4all.nl>
2015-12-04 16:43:18 +0100

    Move configuration related files to their own subdirectory and add the beginnings of an ignore system.

---
+++ MeidoBot/Configuration/Ignores.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+
+namespace MeidoBot
+{
+    class Ignores
+    {
+        public int Count
+        {
+            get
+            {
+                if (ignores != null)
+                    return ignores.Count;
+                else
+                    return 0;
+            }
+        }
+
+        readonly HashSet<string> ignores;
+
+
+        public Ignores() : this(null) {}
+
+        public Ignores(IEnumerable<string> ignoreSources)
+        {
+            if (ignoreSources != null)
+            {
+                ignores = new HashSet<string>(ignoreSources, StringComparer.OrdinalIgnoreCase);
+                ignores.TrimExcess();
+            }
+        }
+
+        public static Ignores FromFile(string path, Logger log)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+                throw new ArgumentException("Cannot be null, empty or whitespace.", "path");
+
+            string[] lines;
+            try
+            {
+                lines = File.ReadAllLines(path);
+            }
+            catch (IOException ex)
+            {
+                log.Verbose("Error loading ignores from {0} ({1})", path, ex.Message);
+                return new Ignores();
+            }
+
+            var tmp = new List<string>(lines.Length);
+            foreach (string line in lines)
+            {
+                var trimmed = line.Trim();
+                if (!string.IsNullOrEmpty(trimmed))
+                    tmp.Add(trimmed);
+            }
+
+            var ignores = new Ignores(tmp);
+            log.Message("Loaded {0} ignores from {1}", ignores.Count, path);
+            return ignores;
+        }
+
+
+        public bool Contains(string source)
+        {
+            if (ignores != null)
+                return ignores.Contains(source);
+            else
+                return false;
+        }
+    }
+}
