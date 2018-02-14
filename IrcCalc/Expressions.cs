0f7a6df3864bb6459b58f3df97648e633413b42a
Ivion <ivion@xs4all.nl>
2018-02-14 21:19:22 +0100

    IrcCalc: Finish the new expressions
    And remove the old one.

--- IrcCalc/Expressions.cs
+++ IrcCalc/Expressions.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
@@ -12,23 +13,27 @@ namespace Calculation
         public readonly string ErrorMessage;
         public readonly int ErrorPosition;
 
+        internal T[] BackingArray;
+
 
         public GenericExpression(IEnumerable<T> expr)
         {
             if (expr == null)
-                throw new System.ArgumentNullException(nameof(expr));
+                throw new ArgumentNullException(nameof(expr));
             
             Success = true;
-            Expression = new ReadOnlyCollection<T>( expr.ToArray() );
+            BackingArray = expr.ToArray();
+            Expression = new ReadOnlyCollection<T>(BackingArray);
         }
 
         public GenericExpression(T[] expr)
         {
             if (expr == null)
-                throw new System.ArgumentNullException(nameof(expr));
+                throw new ArgumentNullException(nameof(expr));
             
             Success = true;
-            Expression = new ReadOnlyCollection<T>(expr);
+            BackingArray = expr;
+            Expression = new ReadOnlyCollection<T>(BackingArray);
         }
 
 
@@ -50,6 +55,16 @@ namespace Calculation
         internal TokenExpression(string errorMsg) : base(errorMsg) {}
 
         internal TokenExpression(string errorMsg, int errorPos) : base(errorMsg, errorPos) {}
+
+
+        public static TokenExpression Parse(string expr)
+        {
+            if (expr == null)
+                throw new ArgumentNullException(nameof(expr));
+            
+            var t = new Tokenizer();
+            return t.Tokenize(expr);
+        }
     }
 
 
@@ -58,5 +73,31 @@ namespace Calculation
         internal VerifiedExpression(CalcToken[] tokens) : base(tokens) {}
 
         internal VerifiedExpression(string errorMsg, int errorPos) : base(errorMsg, errorPos) {}
+
+
+        public static VerifiedExpression Parse(string expr, CalcEnvironment env)
+        {
+            if (env == null)
+                throw new ArgumentNullException(nameof(env));
+            
+            var tokenExpr = TokenExpression.Parse(expr);
+            if (tokenExpr.Success)
+                return Verifier.VerifyExpression(tokenExpr.BackingArray, env);
+            else
+                return new VerifiedExpression(tokenExpr.ErrorMessage, tokenExpr.ErrorPosition);
+        }
+
+        public static VerifiedExpression Create(TokenExpression tokenExpr, CalcEnvironment env)
+        {
+            if (tokenExpr == null)
+                throw new ArgumentNullException(nameof(tokenExpr));
+            if (!tokenExpr.Success)
+                throw new ArgumentException("Expression must be successfully parsed.", nameof(tokenExpr));
+            if (env == null)
+                throw new ArgumentNullException(nameof(env));
+            
+            var arr = tokenExpr.Expression.ToArray();
+            return Verifier.VerifyExpression(arr, env);
+        }
     }
 }
