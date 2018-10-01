0a64dfdf5cc08782625ad3d8da85e645510e4118
Ivion <ivion@xs4all.nl>
2018-10-01 21:54:34 +0200

    MeidoCommon: Update IMeidoComm
    Expose config watching and add some overdue documentation.

--- MeidoCommon/CommInterfaces.cs
+++ MeidoCommon/CommInterfaces.cs
@@ -4,18 +4,51 @@ namespace MeidoCommon
 {
     public interface IMeidoComm
     {
+        /// <summary>
+        /// Full path of the configuration directory.
+        /// </summary>
         string ConfDir { get; }
+        /// <summary>
+        /// Full path of the data directory.
+        /// </summary>
         string DataDir { get; }
 
+        /// <summary>
+        /// Gives full path to configuration file.
+        /// </summary>
+        /// <returns>Full path to the configuration file.</returns>
+        /// <param name="filename">Filename.</param>
         string ConfPathTo(string filename);
+        /// <summary>
+        /// Gives full path to data file.
+        /// </summary>
+        /// <returns>Full path to the configuration file.</returns>
+        /// <param name="filename">Filename.</param>
         string DataPathTo(string filename);
 
+        /// <summary>
+        /// Creates a logger instance for Meidobot plugin.
+        /// </summary>
+        /// <returns>The logger.</returns>
+        /// <param name="plugin">Plugin.</param>
         ILog CreateLogger(IMeidoHook plugin);
 
         void RegisterTrigger(string trigger, Action<IIrcMessage> callback);
         void RegisterTrigger(string trigger, Action<IIrcMessage> callback, bool needChannel);
 
-        bool Auth(string nick, string pass);
+        /// <summary>
+        /// Loads configuration and watches the file for changes. The function gets called with the full path of the
+        /// configuration file.
+        /// </summary>
+        /// <param name="filename">Filename.</param>
+        /// <param name="loadConfig">Function that loads the configuration from passed path.</param>
+        void LoadAndWatchConfig(string filename, Action<string> loadConfig);
+
+        /// <summary>
+        /// Gives authentication level for nick.
+        /// </summary>
+        /// <returns>The authentication level.</returns>
+        /// <param name="nick">Nick.</param>
         int AuthLevel(string nick);
     }
 
