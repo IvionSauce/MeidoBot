589496ef87fb36cfde6fc1a44b58afbc1218313d
Ivion <ivion@xs4all.nl>
2018-10-03 00:59:05 +0200

    MeidoBot: Encapsulate the UserAuth dictionary
    Both for clarity and seperation of concerns, but also to ensure it's
    only used in a readonly fashion (after it has been instantiated).

---
+++ MeidoBot/Authentication/AuthDictionary.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace MeidoBot
+{
+    class AuthDictionary
+    {
+        readonly Dictionary<string, UserAuth> auths;
+
+
+        public AuthDictionary(XElement xml)
+        {
+            auths = new Dictionary<string, UserAuth>(StringComparer.OrdinalIgnoreCase);
+
+            foreach (XElement entry in xml.Elements("entry"))
+            {
+                ParseEntry(entry);
+            }
+        }
+
+        void ParseEntry(XElement entry)
+        {
+            var pass = (string)entry.Element("pass");
+            int level = ParseLevel(entry.Element("level"));
+
+            foreach (XElement xnick in entry.Elements("nick"))
+            {
+                var nick = (string)xnick;
+
+                if (!string.IsNullOrWhiteSpace(nick) && !string.IsNullOrWhiteSpace(pass))
+                {
+                    auths[nick] = new UserAuth(pass, level);
+                }
+            }
+        }
+
+        static int ParseLevel(XElement el)
+        {
+            int level;
+            if (el != null && int.TryParse(el.Value, out level))
+                return level;
+            else
+                return 0;
+        }
+
+
+        public bool TryGet(string nick, out UserAuth user)
+        {
+            return auths.TryGetValue(nick, out user);
+        }
+
+
+        public static XElement DefaultConfig()
+        {
+            var config =
+                new XElement("auth",
+                             new XElement("entry",
+                                          new XElement("nick"),
+                                          new XElement("level"),
+                                          new XElement("pass")
+                                         )
+                            );
+
+            return config;
+        }
+    }
+}
--- MeidoBot/Authentication/UserAuthManager.cs
+++ MeidoBot/Authentication/UserAuthManager.cs
@@ -9,76 +9,25 @@ namespace MeidoBot
     class UserAuthManager
     {
         readonly Logger log;
-
-        volatile Dictionary<string, UserAuth> auths =
-            new Dictionary<string, UserAuth>(StringComparer.OrdinalIgnoreCase);
-
-        static readonly XElement defaultConfig =
-            new XElement("auth",
-                new XElement("entry",
-                    new XElement("nick"),
-                    new XElement("level"),
-                    new XElement("pass")
-                )
-            );
+        volatile AuthDictionary auths;
 
 
         public UserAuthManager(string filename, WatchConfig watcher, Logger log)
         {
             this.log = log;
-            var xmlConf = new XmlConfig2< Dictionary<string, UserAuth> >(
-                defaultConfig,
-                Parse,
+            // Setting up configuration.
+            var xmlConf = new XmlConfig2<AuthDictionary>(
+                AuthDictionary.DefaultConfig(),
+                (xml) => new AuthDictionary(xml),
                 log,
                 Configure
             );
             watcher.LoadAndWatch(filename, xmlConf.LoadConfig);
         }
 
-        void Configure(Dictionary<string, UserAuth> authDict)
+        void Configure(AuthDictionary dict)
         {
-            auths = authDict;
-        }
-
-
-        Dictionary<string, UserAuth> Parse(XElement xml)
-        {
-            var authDict = new Dictionary<string, UserAuth>(StringComparer.OrdinalIgnoreCase);
-
-            foreach (XElement entry in xml.Elements("entry"))
-            {
-                ParseEntry(entry, authDict);
-            }
-
-            return authDict;
-        }
-
-        void ParseEntry(XElement entry, Dictionary<string, UserAuth> dict)
-        {
-            var pass = (string)entry.Element("pass");
-            int level = ParseLevel(entry.Element("level"));
-
-            foreach (XElement xnick in entry.Elements("nick"))
-            {
-                var nick = (string)xnick;
-
-                if (!string.IsNullOrWhiteSpace(nick) && !string.IsNullOrWhiteSpace(pass))
-                {
-                    var user = new UserAuth(pass, level);
-
-                    log.Verbose("Registering user '{0}' with level {1}.", nick, user.Level);
-                    dict[nick] = user;
-                }
-            }
-        }
-
-        static int ParseLevel(XElement el)
-        {
-            int level;
-            if (el != null && int.TryParse(el.Value, out level))
-                return level;
-            else
-                return 0;
+            auths = dict;
         }
 
 
@@ -86,7 +35,7 @@ namespace MeidoBot
         {
             UserAuth user;
             bool success = false;
-            if (auths.TryGetValue(nick, out user))
+            if (auths.TryGet(nick, out user))
                 success = user.Authenticate(pass);
                 
             if (success)
@@ -100,7 +49,7 @@ namespace MeidoBot
         public int AuthLevel(string nick)
         {
             UserAuth user;
-            if (auths.TryGetValue(nick, out user))
+            if (auths.TryGet(nick, out user))
             {
                 /* Ideally I'd want a Authentication system that could leverage the NickServ registration already
                  * in place on many IRC servers. Sadly SmartIrc4Net's IrcUser.IsRegistered doesn't update the
