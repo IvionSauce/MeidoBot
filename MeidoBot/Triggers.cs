83e487b1e62a01a88a8d051e2285a19d5f36282d
Ivion <ivion@xs4all.nl>
2018-10-05 23:58:17 +0200

    MeidoBot: Put final touches on Triggers

--- MeidoBot/Triggers.cs
+++ MeidoBot/Triggers.cs
@@ -7,7 +7,9 @@ namespace MeidoBot
 {
     class Triggers
     {
-        readonly Dictionary<string, Trigger> triggers = new Dictionary<string, Trigger>(StringComparer.Ordinal);
+        public event Action< IEnumerable<Trigger> > PluginTriggersRegister;
+
+        readonly Dictionary<string, Trigger> triggers;
 
         readonly ThrottleManager throttle;
         readonly Logger log;
@@ -17,11 +19,34 @@ namespace MeidoBot
         {
             throttle = tManager;
             this.log = log;
+
+            triggers = new Dictionary<string, Trigger>(StringComparer.Ordinal);
         }
 
 
-        public void RegisterTrigger(string identifier, Trigger trigger)
+        public void RegisterTriggers(IEnumerable<Trigger>[] allTriggers)
         {
+            foreach (IEnumerable<Trigger> trigs in allTriggers)
+            {
+                RegisterTriggers(trigs);
+                if (PluginTriggersRegister != null)
+                {
+                    PluginTriggersRegister(trigs);
+                }
+            }
+        }
+
+        public void RegisterTriggers(IEnumerable<Trigger> triggersPerPlugin)
+        {
+            foreach (Trigger trig in triggersPerPlugin)
+            {
+                RegisterTrigger(trig);
+            }
+        }
+
+        public void RegisterTrigger(Trigger trigger)
+        {
+            string identifier = trigger.Identifier;
             switch (identifier)
             {
                 case "h":
@@ -44,6 +69,16 @@ namespace MeidoBot
         }
 
 
+        public TriggerThreading GetThreading(string identifier)
+        {
+            Trigger trigger;
+            if (triggers.TryGetValue(identifier, out trigger))
+                return trigger.Threading;
+
+            return TriggerThreading.Default;
+        }
+
+
         public void FireTrigger(IIrcMessage msg)
         {
             Trigger tr;
@@ -52,22 +87,22 @@ namespace MeidoBot
                 string source = msg.Channel ?? "PM";
                 log.Message("{0}/{1} {2}", source, msg.Nick, msg.Message);
 
-                if (FirePredicate(msg, tr))
+                if (FirePredicate(msg, tr.Option))
                     tr.Call(msg);
             }
         }
 
-        bool FirePredicate(IIrcMessage msg, Trigger trigger)
+        bool FirePredicate(IIrcMessage msg, TriggerOption opt)
         {
             bool fire = true;
-            switch (trigger.Options)
+            switch (opt)
             {
-                case TriggerOptions.ChannelOnly:
+                case TriggerOption.ChannelOnly:
                 if (msg.Channel == null)
                     fire = false;
                 break;
 
-                case TriggerOptions.QueryOnly:
+                case TriggerOption.QueryOnly:
                 if (msg.Channel != null)
                     fire = false;
                 break;
