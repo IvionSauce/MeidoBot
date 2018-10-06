84cad850b308ac7e28a017aef6ae269a49bcf6de
Ivion <ivion@xs4all.nl>
2018-10-06 07:11:37 +0200

    MeidoBot: Add some handy plugin extention methods

---
+++ MeidoBot/PluginExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using MeidoCommon;
+
+
+namespace MeidoBot
+{
+    public static class PluginExtensions
+    {
+        public static string Name(this IMeidoHook plugin)
+        {
+            string name = plugin.Name.Trim();
+            switch (name)
+            {
+                case "":
+                case null:
+                name = "Unknown";
+                break;
+                // Reserved names.
+                case "Main":
+                case "Meido":
+                case "Auth":
+                name = "_" + name;
+                break;
+            }
+
+            return name;
+        }
+
+        public static Dictionary<string, string> Help(this IMeidoHook plugin)
+        {
+            if (plugin.Help != null)
+                return plugin.Help;
+
+            return new Dictionary<string, string>();
+        }
+
+        public static IEnumerable<Trigger> Triggers(this IMeidoHook plugin)
+        {
+            if (plugin.Triggers != null)
+                return plugin.Triggers;
+
+            return new Trigger[0];
+        }
+    }
+}
