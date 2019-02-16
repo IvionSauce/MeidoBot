097353772cb959c7434b356e67efcdd22f6b628f
Ivion <ivion@xs4all.nl>
2019-02-16 21:43:20 +0100

    MiscUtils: Move timer trigger to new class

---
+++ MiscUtils/TimerTrigger.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using MeidoCommon;
+
+
+class TimerTrigger
+{
+    readonly IrcTimers ircTimers = new IrcTimers();
+
+
+    public void Timer(ITriggerMsg e)
+    {
+        var timers = ircTimers.GetTimers(e.Nick);
+
+        // timer
+        if (e.MessageArray.Length == 1)
+        {
+            EmitDescriptions(timers.Descriptions(), e);
+            return;
+        }
+
+        // timer <delta> [index]
+        // Delta is +N or -N
+        if (e.MessageArray[1].Length > 1)
+        {
+            char deltaSign = e.MessageArray[1][0];
+            if (deltaSign == '+' || deltaSign == '-')
+            {
+                var delta = IrcTimers.Parse(e.MessageArray[1]);
+                int index;
+                if (e.MessageArray.Length >= 3)
+                    index = IrcTimers.Parse(e.MessageArray[2], -1);
+                else
+                    index = 0;
+
+                TimerChange(index, delta, e, timers);
+                return;
+            }
+        }
+
+        // timer stop [index]
+        if (e.MessageArray[1] == "stop")
+            TimerStop(e, timers);
+        // timer change <index> <delta>
+        else if (e.MessageArray.Length == 4 && e.MessageArray[1] == "change")
+        {
+            var delta = IrcTimers.Parse(e.MessageArray[3]);
+            int index = IrcTimers.Parse(e.MessageArray[2], -1);
+
+            TimerChange(index, delta, e, timers);
+        }
+
+        // timer <duration> [message]
+        else
+            TimerStart(e, timers);
+    }
+
+
+    static void TimerStart(ITriggerMsg e, Timers timers)
+    {
+        // Return if invalid or negative duration.
+        var duration = IrcTimers.Parse(e.MessageArray[1]);
+        if (duration <= TimeSpan.Zero)
+            return;
+
+        // Grab message if one is given.
+        string message = null;
+        if (e.MessageArray.Length > 2)
+            message = string.Join(" ", e.MessageArray, 2, e.MessageArray.Length - 2);
+
+        int tmrNo = timers.Enqueue(duration, e, message);
+        if (tmrNo >= 0)
+            e.Reply("Your timer has started. [{0}]", tmrNo);
+        else
+            e.Reply("Max timer count reached. Please wait for some timers to finish or stop them manually.");
+    }
+
+    static void TimerChange(int index, TimeSpan delta, ITriggerMsg e, Timers timers)
+    {
+        // Return if invalid or 0 delta.
+        if (delta == TimeSpan.Zero)
+            return;
+
+        TimerDescription desc;
+        if (timers.Change(index, delta, out desc))
+        {
+            if (string.IsNullOrEmpty(desc.Message))
+                e.Reply("Changed timer {0} to {1} ({2}).",
+                        index, desc.Duration.Str(), desc.Remaining.Str());
+            else
+                e.Reply("Changed \"{0}\" to {1} ({2}).",
+                        desc.Message, desc.Duration.Str(), desc.Remaining.Str());
+        }
+        else
+            e.Reply("No such timer.");
+    }
+
+    static void TimerStop(ITriggerMsg e, Timers timers)
+    {
+        // Stop all timers.
+        if (e.MessageArray.Length == 2)
+        {
+            timers.StopAll();
+            e.Reply("Stopped all your timers.");
+        }
+        // Stop one timer.
+        else
+        {
+            int index = IrcTimers.Parse(e.MessageArray[2], -1);
+            TimerDescription desc;
+            if (timers.Stop(index, out desc))
+            {
+                if (string.IsNullOrEmpty(desc.Message))
+                    e.Reply("Stopped timer {0} :: {1}/{2}",
+                            index, desc.Elapsed.Str(), desc.Duration.Str());
+                else
+                    e.Reply("Stopped \"{0}\" :: {1}/{2}",
+                            desc.Message, desc.Elapsed.Str(), desc.Duration.Str());
+            }
+            else
+                e.Reply("No such timer.");
+        }
+    }
+
+
+
+
+
+    static void EmitDescriptions(TimerDescription[] descs, ITriggerMsg msg)
+    {
+        foreach (var desc in descs)
+            EmitDescription(desc, msg);
+        msg.SendNotice(" -----");
+    }
+
+    static void EmitDescription(TimerDescription desc, ITriggerMsg msg)
+    {
+        msg.SendNotice("[{0}] {1} :: {2} ({3})",
+                       desc.Index, desc.Message, desc.Duration.Str(), desc.Remaining.Str());
+    }
+}
