b9b164116493444b2bcbe673799038579dee084d
Ivion <ivion@xs4all.nl>
2019-03-09 16:41:52 +0100

    MeidoBot: Extend TriggerHelp for querying help

--- MeidoBot/Plugins/PluginExtensions.cs
+++ MeidoBot/Plugins/PluginExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using MeidoCommon;
 
 
@@ -33,6 +35,43 @@ namespace MeidoBot
                 return plugin.Help;
 
             return new Dictionary<string, string>();
+        }
+
+
+        public static bool Query(this TriggerHelp root, string[] fullCommand, out CommandHelp help)
+        {
+            help = null;
+            var cmdHelpNodes = root.Commands;
+            foreach (string cmdPart in fullCommand)
+            {
+                if (TryGetHelp(cmdPart, cmdHelpNodes, out help))
+                    cmdHelpNodes = help.Subcommands;
+                else
+                {
+                    help = null;
+                    break;
+                }
+            }
+
+            return help != null;
+        }
+
+        static bool TryGetHelp(
+            string command,
+            ReadOnlyCollection<CommandHelp> searchSpace,
+            out CommandHelp help)
+        {
+            foreach (var cmdHelp in searchSpace)
+            {
+                if (cmdHelp.Command.Equals(command, StringComparison.Ordinal))
+                {
+                    help = cmdHelp;
+                    return true;
+                }
+            }
+
+            help = null;
+            return false;
         }
     }
 }
