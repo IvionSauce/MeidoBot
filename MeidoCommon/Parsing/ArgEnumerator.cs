dce58e0fbbfd5a4c17d9ebad8858e29bcc65ccb6
Ivion <ivion@xs4all.nl>
2020-04-01 23:47:23 +0200

    MeidoCommon: Several approaches to consuming args
    Since I'm completely unsure on which ways work well and/or I
    prefer. Also to postpone having to commit to a certain approach and
    enshrining it in the relevant interface.

---
+++ MeidoCommon/Parsing/ArgEnumerator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace MeidoCommon.Parsing
+{
+    public class ArgEnumerator : IDisposable
+    {
+        public string CurrentArg;
+
+        readonly IEnumerator<string> argNumerator;
+
+
+        public ArgEnumerator(IEnumerable<string> argv)
+        {
+            if (argv == null)
+                throw new ArgumentNullException(nameof(argv));
+
+            argNumerator = argv.GetEnumerator();
+            CurrentArg = string.Empty;
+        }
+
+
+        public bool NextArg()
+        {
+            bool retval = false;
+
+            while (!retval)
+            {
+                bool nextSucces = argNumerator.MoveNext();
+                if (nextSucces)
+                {
+                    if (!string.IsNullOrWhiteSpace(argNumerator.Current))
+                    {
+                        CurrentArg = argNumerator.Current;
+                        retval = true;
+                    }
+                }
+                else
+                {
+                    CurrentArg = string.Empty;
+                    break;
+                }
+            }
+
+            return retval;
+        }
+
+
+        public List<string> GetRemaining()
+        {
+            return GetRemaining(false);
+        }
+
+        public List<string> GetRemaining(bool removeEmptyOrWhitespace)
+        {
+            var remain = new List<string>();
+            while (argNumerator.MoveNext())
+            {
+                if ( !(string.IsNullOrWhiteSpace(argNumerator.Current) && removeEmptyOrWhitespace) )
+                    remain.Add(argNumerator.Current);
+            }
+
+            CurrentArg = string.Empty;
+            return remain;
+        }
+
+
+        public void Reset()
+        {
+            argNumerator.Reset();
+        }
+
+        public void Dispose()
+        {
+            argNumerator.Dispose();
+        }
+    }
+}
---
+++ MeidoCommon/Parsing/GetArgs.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace MeidoCommon.Parsing
+{
+    public static class ParseArgs
+    {
+        public static string ArgString(this IIrcMsg msg)
+        {
+            return string.Join(" ", ArgArray(msg));
+        }
+
+        public static string[] ArgArray(this IIrcMsg msg)
+        {
+            if (msg == null)
+                throw new ArgumentNullException(nameof(msg));
+
+            string[] arguments = null;
+            var argv = msg.Message.Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+
+            if (argv.Length > 1)
+            {
+                arguments = new string[argv.Length - 1];
+                Array.Copy(argv, 1, arguments, 0, arguments.Length);
+            }
+
+            return arguments ?? new string[0];
+        }
+
+
+        public static IEnumerable<string> GetArg(this IIrcMsg msg, out string argument)
+        {
+            if (msg == null)
+                throw new ArgumentNullException(nameof(msg));
+
+            return GetArg(msg.MessageArray.Skip(1), out argument);
+        }
+
+        public static IEnumerable<string> GetArg(this IEnumerable<string> argv, out string argument)
+        {
+            if (argv == null)
+                throw new ArgumentNullException(nameof(argv));
+
+            argument = null;
+            int skipCount = 0;
+
+            foreach (var arg in argv)
+            {
+                skipCount++;
+                if (!string.IsNullOrWhiteSpace(arg))
+                {
+                    argument = arg;
+                    break;
+                }
+            }
+
+            return argv.Skip(skipCount);
+        }
+
+
+        public static string GetArg(this IIrcMsg msg, out List<string> rest)
+        {
+            var tmp = GetArgs(msg, 1, out rest);
+            return tmp[0];
+        }
+
+        public static string[] GetArgs(this IIrcMsg msg, int count)
+        {
+            return GetArgs(msg, count, out List<string> rest);
+        }
+
+        public static string[] GetArgs(this IIrcMsg msg, int count, out List<string> rest)
+        {
+            if (msg == null)
+                throw new ArgumentNullException(nameof(msg));
+
+            return GetArgs(msg.MessageArray.Skip(1), count, out rest);
+        }
+
+
+        public static string[] GetArgs(this IEnumerable<string> argv, int count, out List<string> rest)
+        {
+            if (argv == null)
+                throw new ArgumentNullException(nameof(argv));
+            if (count < 1)
+                throw new ArgumentOutOfRangeException(nameof(count), "Cannot be 0 or negative.");
+
+            var arguments = new string[count];
+            rest = null;
+
+            using (var argEnum = new ArgEnumerator(argv))
+            {
+                int idx = 0;
+
+                while (idx < count && argEnum.NextArg())
+                {
+                    arguments[idx] = argEnum.CurrentArg;
+                }
+
+                if (idx == (count - 1))
+                    rest = argEnum.GetRemaining();
+                else
+                    arguments = null;
+            }
+
+            return arguments;
+        }
+    }
+}
