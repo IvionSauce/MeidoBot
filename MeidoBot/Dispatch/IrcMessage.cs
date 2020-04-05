53184d52a5af2786d605311bf7067431762b10dc
Ivion <ivion@xs4all.nl>
2020-04-05 21:20:16 +0200

    MeidoBot: Preparations to ditch MessageArray
    Prefer an immutable data structure, you can't trust plugins to not alter
    the array - even if I've always taken care never to do that.

--- MeidoBot/Dispatch/IrcMessage.cs
+++ MeidoBot/Dispatch/IrcMessage.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.ObjectModel;
 using Meebey.SmartIrc4net;
 // Using directives for plugin use.
 using MeidoCommon;
@@ -9,9 +10,18 @@ namespace MeidoBot
     class IrcMsg : IQueryMsg, IChannelMsg, ITriggerMsg, IQueryAction, IChannelAction
     {
         public IIrcComm Irc { get; private set; }
-        // Identical to the IrcMessageData properties.
+        // Parallel to the IrcMessageData properties.
         public string Message { get; private set; }
-        public string[] MessageArray { get; private set; }
+        public string[] MessageArray
+        {
+            get
+            {
+                var array = new string[MessageParts.Count];
+                MessageParts.CopyTo(array, 0);
+                return array;
+            }
+        }
+        public ReadOnlyCollection<string> MessageParts { get; private set; }
         public string Channel { get; private set; }
         public string Nick { get; private set; }
         public string Ident { get; private set; }
@@ -29,7 +39,7 @@ namespace MeidoBot
             // SmartIrc4Net already chops off the control character for us,
             // so use that.
             Message = e.ActionMessage;
-            MessageArray = Message.Split(' ');
+            DoParts(Message.Split(' '));
         }
 
         public IrcMsg(IrcComm irc, IrcMessageData messageData, string prefix)
@@ -38,7 +48,8 @@ namespace MeidoBot
             type = messageData.Type;
 
             Message = messageData.Message;
-            MessageArray = messageData.MessageArray;
+            DoParts(messageData.MessageArray);
+
             Channel = messageData.Channel;
             Nick = messageData.Nick;
             Ident = messageData.Ident;
@@ -48,6 +59,10 @@ namespace MeidoBot
             ReturnTo = Channel ?? Nick;
         }
 
+        void DoParts(string[] backingArray)
+        {
+            MessageParts = new ReadOnlyCollection<string>(backingArray);
+        }
 
         // Returns trigger without the prefix.
         // Will be null if message didn't start with a prefix.
@@ -62,7 +77,7 @@ namespace MeidoBot
 
             if (Message.StartsWith(prefix, StringComparison.Ordinal))
             {
-                string trigger = MessageArray[0].Substring(prefix.Length);
+                string trigger = MessageParts[0].Substring(prefix.Length);
                 // If trigger starts with prefix suppose it wasn't the intent to call a trigger.
                 if (trigger.StartsWith(prefix, StringComparison.Ordinal))
                     return null;
@@ -71,10 +86,11 @@ namespace MeidoBot
             }
 
             if (type == ReceiveType.QueryMessage)
-                return MessageArray[0];
+                return MessageParts[0];
             else
                 return null;
         }
+
 
         public void SendNotice(string message, params object[] args)
         {
