cadfaa4593147e89c2b9f44c3a14655c2cea0d8b
Ivion <ivion@xs4all.nl>
2020-04-06 14:30:37 +0200

    MeidoCommon: Add Subarray extension methods

--- MeidoCommon/ExtensionMethods.cs
+++ MeidoCommon/ExtensionMethods.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 
@@ -12,6 +13,48 @@ namespace MeidoCommon.ExtensionMethods
                 return seq.Where(el => el != null);
             else
                 return Enumerable.Empty<T>();
+        }
+
+
+        public static T[] Subarray<T>(this IList<T> items, int start)
+        {
+            if (items == null)
+                throw new ArgumentNullException(nameof(items));
+            if (start > items.Count)
+                throw new ArgumentOutOfRangeException(nameof(start), "Cannot be greater than items' count.");
+
+            return Subarray(items, start, items.Count - start);
+        }
+
+        public static T[] Subarray<T>(this IList<T> items, int start, int count)
+        {
+            if (items == null)
+                throw new ArgumentNullException(nameof(items));
+            if (start < 0)
+                throw new ArgumentOutOfRangeException(nameof(start), "Cannot be negative.");
+            if (count < 0)
+                throw new ArgumentOutOfRangeException(nameof(count), "Cannot be negative.");
+            if ( (start + count) > items.Count )
+                throw new ArgumentException("Requested range would go out of bounds.");
+            // Early return.
+            if (count == 0)
+                return Array.Empty<T>();
+
+            var retval = new T[count];
+            // If items is actually an array use the efficient Array.Copy.
+            if (items is T[] arr)
+            {
+                Array.Copy(arr, start, retval, 0, count);
+            }
+            else
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    retval[i] = items[start + i];
+                }
+            }
+
+            return retval;
         }
     }
 }
